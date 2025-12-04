30734f0d674937ce590764f82f7349dff7983aec
asyrafsarkei <muhamadasyraf.sarkei@gmail.com>
2025-12-04 12:57:56 +0800

    Update game.js

--- game.js
+++ game.js
@@ -99,6 +99,7 @@ function startGameUI(userData) {
 
             <div class="scene-container" id="waterScene">
                 <div class="sun">☀️</div>
+                <div id="darkCloud" style="position: absolute; top: -100px; left: -100px; font-size: 100px; opacity: 0; color: #444444; z-index: 50;">☁️</div>
                 <div class="cloud" id="cloudMain" style="opacity:0.3; top: 30px; right: 50px; font-size: 60px;">☁️</div>
                 <div class="cloud" id="cloudSmall1" style="opacity:0.2; top: 80px; left: 150px; font-size: 30px;">☁️</div>
                 <div class="cloud" id="cloudSmall2" style="opacity:0.2; top: 50px; right: 10px; font-size: 40px;">☁️</div>
